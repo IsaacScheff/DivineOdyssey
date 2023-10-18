4fed2fb3244afabdfd04bc68ed9c64b793def6b2
Isaac Scheff <isaac1996@comcast.net>
2023-10-17 21:43:39 -0400

    Added stats to BaseUnit

--- Assets/Scripts/Units/BaseUnit.cs
+++ Assets/Scripts/Units/BaseUnit.cs
@@ -6,5 +6,14 @@ public class BaseUnit : MonoBehaviour {
     public string UnitName;
     public Tile OccupiedTile;
     public Faction Faction;
+    public int Movement;
+    public int Health;
+    public int Strength;
+    public int Ego;
+    public int Spirit;
+    public int Body;
+    public int Grit;
+    public int Resilience;
   
 }
+
